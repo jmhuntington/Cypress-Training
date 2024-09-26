c2dfbe7d10146c617a65a9de64c8ea37d18e81a6
jmhuntington <84036158+jmhuntington@users.noreply.github.com>
2024-09-26 11:56:35 -0700

    Update spec.cy.js

--- e2e/spec.cy.js
+++ e2e/spec.cy.js
@@ -5,9 +5,9 @@ it("my first test", () => {
     cy.get('#username');
     cy.get('[name=password]');
     cy.get('#username').clear();
-    cy.get('#username').type('success+janaqasc@simulator.amazonses.com');
+    cy.get('#username').type('success+blank@simulator.amazonses.com');
     cy.get('#sign-in-password').clear();
-    cy.get('#sign-in-password').type('eSubGodM0deEng@aged!');
+    cy.get('#sign-in-password').type('hidden!');
     cy.get('[data-testid="btn-sign-in"]').click();
     cy.get('[data-testid="page-header-title"]').contains('My Projects (131)');
     cy.get('#Row0Button > [data-testid="ColumnnumberCell0"]').click();
